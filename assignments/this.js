d3fd37897f4d28ea434ac49ea51dfe95e34fb0f6
Brock Falfas <brockfalfas@gmail.com>
2019-03-06 19:33:03 -0500

    another update to question

--- assignments/this.js
+++ assignments/this.js
@@ -4,7 +4,7 @@
 * 1. Whenever a function is contained in the global scope, the value of this inside of that function will be the window object.
 * 2. Whenever a function is called by a preceding dot, the object before that dot is this.
 * 3. Whenever a constructor function is used, this refers to the specifc instance of the object that is created and returned by the constructor function.
-* 4. Whenever Javascipt's call or app method is used, this is explicitly defined.
+* 4. Whenever Javascipt's call or apply method is used, this is explicitly defined.
 *
 * write out a code example of each explanation above
 */
